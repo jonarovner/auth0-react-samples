51cecd3f0b9d62d49933126bb22583551e08b9de
jonarovner <jonathan.rovner@okta.com>
2022-06-12 10:12:29 -0300

    first commit

--- Sample-01/src/components/Footer.js
+++ Sample-01/src/components/Footer.js
@@ -2,9 +2,12 @@ import React from "react";
 
 const Footer = () => (
   <footer className="bg-light p-3 text-center">
-    <div className="logo" />
+  
     <p>
       Sample project provided by <a href="https://auth0.com">Auth0</a>
+    </p>
+    <p>
+      Adapted by Jona Rovner
     </p>
   </footer>
 );
